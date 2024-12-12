1f74e12327afe1aa99dd9c28861f7e7f75c05837
MounirMagedMounir <115494921+MounirMagedMounir@users.noreply.github.com>
2024-12-12 16:05:06 +0200

    initalize file stracture core, share ,pages and feature and adding initial components and templates for login, register, user, role, and table features

---
+++ src/app/pages/verification/verification-email/verification-email.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verification-email',
+  imports: [],
+  templateUrl: './verification-email.component.html',
+  styleUrl: './verification-email.component.scss'
+})
+export class VerificationEmailComponent {
+
+}
