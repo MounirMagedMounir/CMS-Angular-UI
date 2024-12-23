188501f7a6ac96a32b326b232aa2cd3ffa0ebb7f
MounirMagedMounir <115494921+MounirMagedMounir@users.noreply.github.com>
2024-12-23 11:44:52 +0200

    add primeNG library to project and add to provider and config and add styless change favicon

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),provideAnimations(),  provideAnimationsAsync(),provideHttpClient(),providePrimeNG({
+    theme: {
+        preset: Aura
+    }
+})],
 };
