f46c9d5da480350882a4d7e992be8692e250dcb9
MounirMagedMounir <115494921+MounirMagedMounir@users.noreply.github.com>
2024-12-29 11:50:26 +0200

    adding the nav bar to the layput and geting the user date when reloading the pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./share/navbar/navbar.component";
+import { AuthenticationService } from './core/services/authentication/authentication.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'CMS-Angular-UI';
+  constructor(private auth: AuthenticationService) { }
+
+  ngOnInit(): void {
+    var token = localStorage.getItem("token");
+    var refreshToken = localStorage.getItem("refreshToken");
+    if (token && refreshToken) {
+      this.auth.login(token, refreshToken);
+    }
+  }
 }
