3138b70bb6896b2d43c3da028c831a9ce00ce580
MounirMagedMounir <115494921+MounirMagedMounir@users.noreply.github.com>
2024-12-29 11:48:28 +0200

    adding signout , forgetpassword and profile routes and changing the main route to home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,14 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { VerificationEmailComponent } from './pages/verification/verification-email/verification-email.component';
+import { SignoutComponent } from './pages/signout/signout.component';
+import { ForgetpasswordComponent } from './pages/forgetpassword/forgetpassword.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: AppComponent
+        component: HomeComponent
     },
     {
         path:'login',
@@ -22,9 +25,22 @@ export const routes: Routes = [
         component: VerificationEmailComponent
     },
     {
+        path:'signout',
+        component: SignoutComponent
+    },
+    {
+        path:'forgetpassword',
+        component: ForgetpasswordComponent
+    },
+    {
 
         path:'admin',
         loadChildren:()=>import( './pages/admin/admin.module').then(m=>m.AdminModule)   
+    },
+    {
+
+        path:'profile',
+        loadChildren:()=>import( './pages/profile/profile.module').then(m=>m.ProfileModule)   
     }
     
 
