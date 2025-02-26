c8645edde062a4b352d7784dab0d6ae2e6205cb7
MounirMagedMounir <115494921+MounirMagedMounir@users.noreply.github.com>
2025-02-26 14:43:51 +0200

    creating perimission filter interface for role filter interface

---
+++ src/app/features/permissions/interface/permission-filter.ts
@@ -0,0 +1,14 @@
+export interface PermissionFilter 
+    {
+        id: string | null;
+        createdDateFrom:  Date | null;
+        createdDateTo:  Date | null;
+        lastUpdatedDateFrom:  Date | null;
+        lastUpdatedDateTo:  Date | null;
+        createdbyId: string | null;
+        createdbyName: string | null;
+        lastUpdatedbyId: string | null;
+        lastUpdatedbyName: string | null;
+        name: string | null
+      }
+
